0e4ca44e2a2ceb9ce61a823eee743c35bab30d37
shane <rshane.dev@gmail.com>
2024-10-29 21:23:16 +0400

    complet footer component and init hero

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,14 +7,27 @@ const Footer = () => {
   return (
     <footer id='footer'>
       <div className='container'>
-        <div className='right'>
-          <h2 className='title'>{data.contact}</h2>
-          <span>
-            <p>{data.description}</p>
-          </span>
+        <div className='top'>
+          {' '}
+          <div className='right'>
+            <h2 className='title'>{data.contact}</h2>
+            <span>
+              <p>{data.description}</p>
+            </span>
+          </div>
+          <div className='left'>
+            <ContactForm data={data} />
+          </div>
         </div>
-        <div className='left'>
-          <ContactForm data={data} />
+
+        <div className='bottom'>
+          <h3 className='bottom--name'>adamkeyes</h3>
+          <div className='bottom--socials'>
+            <div className='facebook'>F</div>
+            <div className='facebook'>F</div>
+            <div className='facebook'>F</div>
+            <div className='facebook'>F</div>
+          </div>
         </div>
       </div>
     </footer>
